4057ff98be8c9511b516a63d1f6fffccec27affc
Poorna Shri <poornashri2703@gmail.com>
2024-01-19 21:12:53 +0530

    TASK:3:PASSWORD GENERATOR

---
+++ TASK3/PasswordGenerator.py
@@ -0,0 +1,26 @@
+import random
+import string
+
+def GeneratePassword(length):
+    characters = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(random.choice(characters) for i in range(length))
+    return password
+
+def PasswordGenerator():
+    print("------------Password Generator----------------")
+
+    #usinf error handling
+    try:
+        length = int(input("Enter the length of the password: "))
+        if length <= 0:
+            print("Please Enter a positive length")
+            return
+
+    except ValueError:
+        print("Invalit input.Pleae enter a valid number")
+        return
+
+    password = GeneratePassword(length)  
+    print(f"Generated Password: {password}")
+
+PasswordGenerator()    
