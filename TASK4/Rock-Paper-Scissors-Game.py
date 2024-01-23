b01f36e02f380b40f0cd5231f18991f63a126604
Poorna Shri <poornashri2703@gmail.com>
2024-01-23 12:57:41 +0530

    task

---
+++ TASK4/Rock-Paper-Scissors-Game.py
@@ -0,0 +1,9 @@
+import random;
+
+user_score = 0
+computer_score = 0
+
+while True:
+    print("Welcome to Rock-Paper-Scissors Game!")
+    user_score = input("Choose: rock, paper, or scissors:")
+    computer_score = random.choice([''])
